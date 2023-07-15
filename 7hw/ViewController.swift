2ef946087ff1937ac58d2c2dd35c9fe8a2e1c7bc
Aker <@akerke.n@inbox.ru>
2023-07-15 08:02:36 +0600

    Initial Commit

---
+++ 7hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7hw
+//
+//  Created by Akerke on 15.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
